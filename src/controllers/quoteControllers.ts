2b9432c2f6e4adc9450665edcf91d8317ea02d26
plegulluche <plegulluche@gmail.com>
2023-11-28 11:33:40 +0100

    [ADD] adding some coments on quoteController.ts

--- src/controllers/quoteControllers.ts
+++ src/controllers/quoteControllers.ts
@@ -1,10 +1,21 @@
 import puppeteer from "puppeteer";
+
 import { Url } from "src/types/url";
 
+/**
+ * Generates a PDF from a given webpage URL while excluding certain elements.
+ * @param {Url} urlObj - The Url object representing the webpage URL.
+ * @returns {Promise<Buffer>} - A promise that resolves to a buffer containing the PDF data.
+ */
 export async function generatePDF(url: Url): Promise<Buffer> {
+    
+  // Launch a new browser instance using Puppeteer.
   const browser = await puppeteer.launch();
+
+  // Open a new page in the browser.
   const page = await browser.newPage();
 
+  // Navigate to the URL. The toString() method of the Url class is used to get the actual URL string.
   await page.goto(url.toString(), { waitUntil: "networkidle0" });
 
   // Hide or remove the header and footer before generating the PDF
@@ -16,7 +27,13 @@ export async function generatePDF(url: Url): Promise<Buffer> {
     if (footer) footer.style.display = "none";
   });
 
+  // Generate a PDF of the current page and store it in a buffer.
+  // The format 'A4' is used here, but this can be adjusted as needed.
   const pdfBuffer = await page.pdf({ format: "A4" });
+
+  // Close the browser instance.
   await browser.close();
+
+  // Return the PDF data as a buffer.
   return pdfBuffer;
 }
