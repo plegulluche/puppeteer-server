dc28df817471211f22b1f68a811b1c11777eaf55
plegulluche <plegulluche@gmail.com>
2023-11-28 10:43:49 +0100

    [ADD] adding coments on quotesroutes and using the new ulr object

--- src/routes/quotesRoutes.ts
+++ src/routes/quotesRoutes.ts
@@ -1,26 +1,44 @@
-import express, { Request, Response } from 'express'
+import express from 'express';
 
 import generatePDF from 'src/controllers/quoteControllers';
+import { Url } from 'src/types/url';
 
 
-
+// Create a router object from Express. 
+// This router will handle all requests coming to the '/quotes' endpoint.
 const router = express.Router();
 
-
-router.post('/generate-pdf', async (req: Request, res: Response) => {
+// Define a POST route for '/generate-pdf'.
+// This route will be used to receive a URL and return a PDF generated from that URL.
+router.post('/generate-pdf', async (req, res) => {
   try {
-    const { url } = req.body;
-    if (!url) {
+    // Extract the URL from the request body. 
+    // Expecting the client to send a JSON object with a 'url' property.
+    const urlStr = req.body.url;
+    if (!urlStr) {
+      // If no URL is provided in the request, return a 400 Bad Request error.
       return res.status(400).send({ error: 'URL is required' });
     }
 
-    const pdfBuffer = await generatePDF(url);
+    // Create a new Url object using the provided URL string.
+    // The Url class will automatically validate the URL.
+    // If the URL is invalid, it will throw an error which is caught in the catch block.
+    const url = new Url(urlStr);
 
+    // Call the generatePDF function, passing the URL string.
+    // This function is expected to return a PDF file in a Buffer format.
+    const pdfBuffer = await generatePDF(url.toString());
+
+    // Set the Content-Type of the response to 'application/pdf'.
+    // This tells the client that the server is returning a PDF file.
     res.contentType('application/pdf');
+
+    // Send the PDF Buffer as the response.
     res.send(pdfBuffer);
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
 });
+
 
 export default router;
